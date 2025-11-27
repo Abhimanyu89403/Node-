b49e4bf097788b8cfd6aa47f6b8ce1d09d144e95
Abhimanyu Bhosikar <abhimanyubhosikar@gmail.com>
2025-11-27 20:50:01 +0530

    backend path modified

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
-terraform{
-backend "s3" {
-  
-  bucket = "abhibuckettfstate89403-bcde"
-  key = "${var.project_name}/tfstate/terraform.tfstate"
-  encrypt = true
-  region = "ap-south-1"
-}
+terraform {
+  backend "s3" {
+
+    bucket  = "abhibuckettfstate89403-bcde"
+    key     = "-node/tfstate/terraform.tfstate"
+    encrypt = true
+    region  = "ap-south-1"
+  }
 }
